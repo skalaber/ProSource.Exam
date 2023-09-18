48a3cad5cca957b6e9baa39e6bda091ea981c40a
Peter.Ibuyat <peter@proposito.com.au>
2023-09-18 09:42:02 +0800

    Add project files.

---
+++ ProSource.Exam/StringCheck.cs
@@ -0,0 +1,27 @@
+﻿namespace ProSource.Exam
+{
+    public static class StringCheck
+    {
+        public static bool IsEnclosedByAngleBrackets(string s)
+        {
+            int[] openingIndexes = GetIndexes(s, '<');
+            int[] closingIndexes = GetIndexes(s, '>');
+
+            if (openingIndexes.Length != closingIndexes.Length)
+                return false;
+
+            for(int i = 0; i < openingIndexes.Length; i++)
+                if (openingIndexes[i] >= closingIndexes[i])
+                    return false;
+
+            return true;
+        }
+
+        private static int[] GetIndexes(string s, char c)
+        {
+            return Enumerable.Range(0, s.Length)
+             .Where(i => s[i] == c)
+             .ToArray();
+        }
+    }
+}
